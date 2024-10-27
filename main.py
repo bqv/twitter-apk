c3128a35920c6fcb34141ae32432571713f6eade
Tony O <822863+bqv@users.noreply.github.com>
2024-10-27 12:44:22 +0000

    Update main.py

--- main.py
+++ main.py
@@ -43,7 +43,7 @@ def process(latest_version: Version):
 
     print("Downloading patches")
     pikoRelease = download_release_asset(
-        "crimera/piko", "^piko.*jar$", "bins", "patches.jar"
+        "crimera/piko", "^piko.*jar$", "bins", "patches.jar", true
     )
 
     print(pikoRelease["body"])
@@ -54,6 +54,7 @@ def process(latest_version: Version):
         "^rev.*apk$",
         "bins",
         "integrations.apk",
+        true
     )
 
     print(integrationsRelease["body"])
